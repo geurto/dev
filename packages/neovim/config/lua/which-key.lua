c46af03845513b7ad3bb5ea238f54a0387e23f0a
geurto <peter.geurts@proton.me>
2025-03-06 15:34:19 +0100

    no more which-key warnings

--- packages/neovim/config/lua/which-key.lua
+++ packages/neovim/config/lua/which-key.lua
@@ -23,44 +23,10 @@ which_key.setup({
 			g = true,
 		},
 	},
-	window = {
-		border = "single",
-		position = "bottom",
-		margin = { 1, 0, 1, 0 },
-		padding = { 1, 1, 1, 1 },
-	},
 	layout = {
 		height = { min = 4, max = 25 },
 		width = { min = 20, max = 50 },
 		spacing = 3,
 	},
-	ignore_missing = true, -- Set to true to avoid errors on missing mappings
-	hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "^:", "^ ", "^call ", "^lua " },
 	show_help = true,
-	triggers = "auto", -- Explicitly set triggers
-	triggers_nowait = { -- Keys that should trigger immediately
-		-- marks
-		"`",
-		"'",
-		"g`",
-		"g'",
-		-- registers
-		'"',
-		"<c-r>",
-		-- spelling
-		"z=",
-	},
-	triggers_blacklist = {
-		-- list of mode / prefixes that should never be hooked by WhichKey
-		i = { "j", "k" },
-		v = { "j", "k" },
-	},
-})
-
--- Register some basic mappings to avoid errors
-which_key.register({
-	["<leader>f"] = { name = "File" },
-	["<leader>b"] = { name = "Buffer" },
-	["<leader>g"] = { name = "Git" },
-	["<leader>l"] = { name = "LSP" },
 })
