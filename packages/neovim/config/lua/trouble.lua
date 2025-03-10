e1da15f66f8222ceb803cb6b6d3998e1d509b8bb
geurto <peter.geurts@proton.me>
2025-03-10 14:35:08 +0100

    updated trouble

--- packages/neovim/config/lua/trouble.lua
+++ packages/neovim/config/lua/trouble.lua
@@ -1,3 +1,19 @@
+vim.diagnostic.config({
+	virtual_text = true, -- Show diagnostics as virtual text
+	signs = true, -- Show signs in the sign column
+	underline = true, -- Underline the text with an issue
+	update_in_insert = false,
+	severity_sort = true,
+})
+
 require("trouble").setup({
 	focus = true,
 })
+
+-- keymaps
+vim.keymap.set(
+	"n",
+	"<leader>xx",
+	"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+	{ desc = "Buffer Diagnostics (Trouble)", remap = true }
+)
