27e11f38c9b63bdf7662712f6480a22997dcd6e6
geurto <peter.geurts@proton.me>
2025-08-25 13:39:47 +0200

    shortcut to dismiss notifications

--- nvim-config/lua/nvim-notify.lua
+++ nvim-config/lua/nvim-notify.lua
@@ -1,3 +1,9 @@
-require("notify").setup({
+local notify = require("notify")
+
+notify.setup({
 	background_colour = "#000000",
+
+	vim.keymap.set("n", "<leader>pd", function()
+		notify.dismiss()
+	end, { desc = "Dismiss Notify message" }),
 })
