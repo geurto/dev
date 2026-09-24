b90a49076fa12639dffb83305fe32842fdfff614
Peter Geurts <peter.geurts@proton.me>
2026-09-24 09:33:15 +0200

    fixed ts formatting
    Signed-off-by: Peter Geurts <peter.geurts@proton.me>

--- nvim-config/lua/conform.lua
+++ nvim-config/lua/conform.lua
@@ -3,6 +3,9 @@ require("conform").setup({
 		lua = { "stylua" },
 		python = { "ruff_fix", "ruff_format" },
 		javascript = { "prettierd", "prettier", stop_after_first = true },
+		javascriptreact = { "prettierd", "prettier", stop_after_first = true },
+		typescript = { "prettierd", "prettier", stop_after_first = true },
+		typescriptreact = { "prettierd", "prettier", stop_after_first = true },
 		nix = { "nixfmt" },
 		rust = {
 			"rustfmt",
